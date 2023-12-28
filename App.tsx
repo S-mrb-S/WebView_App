bbc9e847991738614a90e4f1aaf3771b635b89ab
MehrabSp <mrbsp2006@gmail.com>
2023-12-28 16:25:27 +0330

    upgrade

--- App.tsx
+++ App.tsx
@@ -1,29 +1,44 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { WebViewComponent } from "./src/WebView";
 import { Starter } from "./src/StartDownload";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { fs, get_per } from "./src/fs";
 
 export default function (): React.JSX.Element {
   const [start, setStart] = useState<boolean>(false);
-  const [todos, setTodos] = useState<[]>([]);
+  const [showStarter, setShowStarter] = useState<boolean>(false);
+  // const [todos, setTodos] = useState<[]>([]);
 
-  // const increment = () => {
-  //   setStart(true);
-  // };
   const changheStart = useCallback(() => {
-    // setTodos((t) => [...t, "New Todo"]);
-    // console.log('changhe')
-    setStart((f) => !f)
-  }, [todos, start]);
+    setStart((f) => !f);
+  }, [start]);
+  const changheShowStarter = useCallback(
+    (val: boolean) => {
+      console.log("granted and : " + val);
+      setShowStarter(val);
+    },
+    [showStarter]
+  );
 
-  console.log(start)
+  if (showStarter) {
+    console.log("starter true!");
+    // fs(
+    //   "https://s.pinimg.com/webapp/HubBanner_mWeb_Beauty-199b84c2.png",
+    //   "1.png"
+    // );
+  }
+
+  useEffect(() => {
+    get_per(changheShowStarter);
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" animated translucent />
       <SafeAreaView style={{ flex: 1 }}>
-        <WebViewComponent />
+        <WebViewComponent start={start} />
         <Starter start={start} Callback={changheStart} />
       </SafeAreaView>
     </View>
---
+++ html_js/index.js
@@ -0,0 +1,143 @@
+// const observer = new MutationObserver((mutations) => {
+//   mutations.forEach((mutation) => {
+//     start_i();
+//     if (mutation.type === "childList") {
+//       const addedNodes = mutation.addedNodes;
+//       for (let i = 0; i < addedNodes.length; i++) {
+//         const addedNode = addedNodes[i];
+//         //   if (addedNode.tagName === 'DIV') {
+//         const images = addedNode.querySelectorAll("img");
+//         images.forEach((image) => {
+//           if (
+//             image.naturalWidth > 100 &&
+//             image.naturalHeight > 100 &&
+//             image.offsetTop < 100
+//           ) {
+//             window.ReactNativeWebView.postMessage(image.src);
+//           }
+//         });
+//         //   }
+//       }
+//     }
+//   });
+// });
+
+// function start_i() {
+//   const images = document.querySelectorAll("img");
+//   for (var i = 0; i < images.length; i++) {
+//     if (
+//       images[i].naturalWidth > 100 &&
+//       images[i].naturalHeight > 100 &&
+//       images[i].offsetTop < 100
+//     ) {
+//       window.ReactNativeWebView.postMessage(images[i].src);
+//     }
+//   }
+// }
+// start_i();
+
+// setTimeout(function () {
+//   window.ReactNativeWebView.postMessage("Start observer");
+//   observer.observe(document.body, { childList: true, subtree: true });
+// }, 500);
+// true; // note: this is required, or you'll sometimes get silent failures
+
+export const WebViewScript_Mutation = `
+// new way
+function sendImages(images) {
+  images.forEach((image) => {
+    if (
+      image.naturalWidth > 100 &&
+      image.naturalHeight > 100 &&
+      image.offsetTop < 100
+    ) {
+      window.ReactNativeWebView.postMessage(image.src);
+    }
+  });
+}
+
+function start_i() {
+  const images = document.querySelectorAll("img");
+  sendImages(images);
+}
+
+start_i();
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    start_i();
+    if (mutation.type === "childList") {
+      const addedNodes = mutation.addedNodes;
+      addedNodes.forEach((addedNode) => {
+        const images = addedNode.querySelectorAll("img");
+        sendImages(images);
+      });
+    }
+  });
+});
+
+setTimeout(function () {
+  window.ReactNativeWebView.postMessage("Start observer");
+  observer.observe(document.body, { childList: true, subtree: true });
+}, 500);
+
+true;
+`;
+
+export const WebViewScript_Intersect = `
+// new way chatgpt3.5
+// دریافت عکس هایی که فقط میبینم
+// توی مرورگر لپ تاپ کار نمیکنه
+function sendImages(images) {
+  images.forEach((image) => {
+    if (
+      image.naturalWidth > 100 &&
+      image.naturalHeight > 100 &&
+      image.offsetTop < 100
+    ) {
+      window.ReactNativeWebView.postMessage(image.src);
+    }
+  });
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const images = entry.target.querySelectorAll("img");
+      sendImages(images);
+    }
+  });
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5, // Adjust the threshold as needed
+};
+
+const imageObserver = new IntersectionObserver(
+  handleIntersect,
+  observerOptions
+);
+
+function start_i() {
+  const images = document.querySelectorAll("img");
+  images.forEach((image) => {
+    imageObserver.observe(image);
+  });
+}
+
+start_i();
+
+setTimeout(function () {
+  window.ReactNativeWebView.postMessage("Start observer");
+}, 500);
+true;
+`;
+
+export const WebViewNoScript = `
+setTimeout(function () {
+  window.ReactNativeWebView.postMessage("No script");
+}, 1000);
+true; // note: this is required, or you'll sometimes get silent failures
+`;
